703beef0fba4da378fd63565313d581495f25cec
vermashiva12 <115514079+vermashiva12@users.noreply.github.com>
2023-03-23 13:54:59 -0400

    check

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -10,7 +10,7 @@ const Appointment = () => {
         <h1 className=' text-center'>Book an Appointment</h1>
        
          </div>
-         <div className='justify-between py-2 mx-[42%]'>
+         <div className='justify-between py-2 mx-[40%]'>
          <a href='https://calendly.com/humblegloriousglass' target='_blank'  rel='noreferrer'>
 
 <button >Click Here to Book</button>
